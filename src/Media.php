ce8013100ac76463f73b7a6e1b02479dc4e6d611
nasser <hekmati.nasser@gmail.com>
2023-06-21 13:32:46 +0330

    fix : path api media

--- src/Media.php
+++ src/Media.php
@@ -3,42 +3,42 @@
 namespace Celysium\Media;
 
 use Celysium\Request\Exceptions\BadRequestHttpException;
-use Exception;
 use Illuminate\Http\UploadedFile;
 use Celysium\Request\Facades\RequestBuilder;
+use Illuminate\Validation\ValidationException;
 
 class Media
 {
     /**
      * @param UploadedFile $file
-     * @return string
+     * @return string|null
      * @throws BadRequestHttpException
      */
-    public function upload(UploadedFile $file): string
+    public function upload(UploadedFile $file): ?string
     {
         return RequestBuilder::request()
             ->attach('file', $file->getContent(), $file->getClientOriginalName())
-            ->post('/v1/media/files')
+            ->post('internal/v1/media/files')
             ->onError(fn($response) => throw new BadRequestHttpException($response))
             ->json('data.path');
     }
 
     /**
      * @param string $url
-     * @return string
-     * @throws Exception
+     * @return string|null
      * @throws BadRequestHttpException
+     * @throws ValidationException
      */
-    public function uploadByUrl(string $url): string
+    public function uploadByUrl(string $url): ?string
     {
         if (trim($url) == '') {
-            throw new Exception("url is empty.");
+            throw ValidationException::withMessages(['url' => ['url is required.']]);
         }
 
         return RequestBuilder::request()
             ->timeout(5)
             ->retry(5)
-            ->post('v1/media/files/url', [
+            ->post('internal/v1/media/files/url', [
                 'url' => $url
             ])
             ->onError(fn($response) => throw new BadRequestHttpException($response))
